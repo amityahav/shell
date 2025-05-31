1d2d49b3c8b2f42b69422b3524272642fca64aba
amityahav <amityahav12@gmail.com>
2025-05-31 18:41:39 +0300

    wip

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::process::exit;
 use std::env;
 use std::fs;
 use std::path::Path;
+use std::process::Command;
 
 fn main() {
     let builtins : HashSet<&str> = vec![
@@ -35,45 +36,78 @@ fn main() {
             },
             "type" => {
                 let command = words[1];
+
+                // first check if this is a sell builtin command.
                 if builtins.contains(command) {
                     println!("{} is a shell builtin", command);
                     continue;
                 }
 
-                let path_var = env::var("PATH").expect("path not set");
-                let paths: Vec<&str> = path_var.split(':').collect();
-                let mut found: bool = false;
+                // second check if this command exists under PATH.
+                match command_in_path_env(command) {
+                    Ok(path_name) => {
+                        if path_name.is_empty() {
+                            println!("{}: not found", command);
+                            continue;
+                        }
 
-                for p in paths {
-                    let path = Path::new(p);
-                    match fs::read_dir(path) {
-                        Ok(entries) => {
-                            for entry in entries {
-                                match entry {
-                                    Ok(ent) => {
-                                        if ent.file_name() == command {
-                                            println!("{} is {}/{}", command, p, command);
-                                            found = true;
-                                            break;
-                                        }
-                                    },
-                                    Err(e) => eprintln!("Failed to read entry: {}", e)
-                                }
-                            }
-                        },
-                        Err(e) => eprintln!("Failed to read directory: {}", e)
-                    }
-
-                    if found {
-                        break
-                    }
-                }
-                                
-                if !found {
-                    println!("{}: not found", command);
+                        println!("{} is {}", command, path_name)
+                    },
+                    Err(e) => eprintln!("{}", e)
                 }
             }
-            &_ => println!("{}: command not found", trimmed_input)
+            &_ => {
+                match command_in_path_env(command) {
+                    Ok(path_name) => {
+                        if path_name.is_empty() {
+                            println!("{}: command not found", trimmed_input);
+                            continue;
+                        }
+
+                        // run the executable and print its stdout,stderr.
+                        let output = Command::new(path_name)
+                        .arg(words[1..].join(" "))
+                        .output();
+
+                        match output {
+                            Ok(out) => {
+                                println!("{}", String::from_utf8_lossy(&out.stdout));
+                                println!("{}", String::from_utf8_lossy(&out.stderr));
+                            },
+                            Err(e) => eprintln!("Failed getting executable output: {}", e)
+                        }
+                    },
+                    Err(e) => eprintln!("{}", e)
+                }
+            }
         }
     }
+}
+
+/// returns the full path of command_name in case it exists in one of the paths under the PATH environment variable.
+/// if no path found, an empty string will be returned.
+fn command_in_path_env(command_name: &str) -> Result<String, String>{
+    let path_var = env::var("PATH").expect("path not set");
+    let paths: Vec<&str> = path_var.split(':').collect();
+
+    for p in paths {
+        let path = Path::new(p);
+        match fs::read_dir(path) {
+            Ok(entries) => {
+                for entry in entries {
+                    match entry {
+                        Ok(ent) => {
+                            if ent.file_name() == command_name {
+                                return Ok( format!("{}/{}", p, command_name));
+                            }
+                        },
+                        Err(e) => return Err(format!("Failed to read entry: {}", e))
+                    }
+                }
+            },
+            Err(e) => return Err(format!("Failed to read directory: {}", e))
+        }
+    }
+                    
+   Ok("".to_string())
 }
