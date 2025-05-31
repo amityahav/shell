c9d8da764190536404c5f031cae235398ad1259a
amityahav <amityahav12@gmail.com>
2025-05-31 16:08:32 +0300

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process::exit;
 
 fn main() {
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
-    
+
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
+
+        if input.trim()  == "exit 0" {
+            exit(0)
+        }
+
         println!("{}: command not found", input.trim())
     }
 }
